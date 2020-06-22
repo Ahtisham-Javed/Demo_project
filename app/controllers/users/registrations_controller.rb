dfd5a1113e75993e383dc209fb4183b6bb6b9add
Ahtisham-Javed <muhammad.ahtisham@devsinc.com>
2020-06-22 13:56:30 +0500

    Revitalized frontend of user account operations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -65,12 +65,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
 private
-def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :user_avatar)
-end
+  def sign_up_params
+      params.require(:user).permit(:name, :address, :email, :password, :password_confirmation, :user_avatar)
+  end
 
-def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :user_avatar)
-end
+  def account_update_params
+      params.require(:user).permit(:name, :address, :email, :password, :password_confirmation, :current_password, :user_avatar)
+  end
 
 end

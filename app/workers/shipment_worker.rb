e5165739b6005ea7c62e55647cdd3eadf46fa8a4
Ahtisham-Javed <muhammad.ahtisham@devsinc.com>
2020-07-13 10:42:21 +0500

    Removed puts statements

--- app/workers/shipment_worker.rb
+++ app/workers/shipment_worker.rb
@@ -2,7 +2,6 @@ class ShipmentWorker
   include Sidekiq::Worker
   
   def perform(user_id, shipments)
-    puts "Sending an email to user"
     UserMailer.product_shipment(user_id, shipments).deliver
   end
 end
